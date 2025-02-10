6aa15d6f929cc4b6b8a29513ebb540817c294cfb
FabioSucameli <fabiosucameli@gmail.com>
2025-02-10 12:17:20 +0100

    Small fixes on sequential code

--- sequential/main.py
+++ sequential/main.py
@@ -11,15 +11,15 @@ def main():
     output_dir = "../output_img"  # Directory di output
     overlay_path = "../cat.png"  # Oggetto da sovrapporre (gatto nero)
 
+    # Inizio timer
+    start_time = time.time()
+
     # Caricamento delle immagini e overlay
     images, image_paths = load_images(input_dir, NUM_IMAGES)
     overlay = cv2.imread(overlay_path, cv2.IMREAD_UNCHANGED)
 
     if overlay is None:
         raise FileNotFoundError("Immagine del gatto non trovata!")
-
-    # Inizio timer
-    start_time = time.time()
 
     # Applicazione della sovrapposizione
     augmented_images = []
@@ -29,13 +29,13 @@ def main():
         augmented_img = overlay_image_alpha(img.copy(), overlay, x_offset, y_offset)
         augmented_images.append(augmented_img)
 
+    # Salvataggio delle immagini modificate nella relativa cartella di output
+    save_images(augmented_images, output_dir)
+
     #Fine timer
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Elaborazione completata in {elapsed_time:.2f} secondi.")
-
-    # Salvataggio delle immagini modificate nella relativa cartella di output
-    save_images(augmented_images, output_dir)
 
 if __name__ == "__main__":
     main()
